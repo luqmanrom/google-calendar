701ab821a11e50afc107cf8db0a88bc77cce0dba
Luqman <luqmanrom@gmail.com>
2017-04-13 21:13:25 +0800

    Add publish config and missing dep

--- src/GoogleCalendarServiceProvider.php
+++ src/GoogleCalendarServiceProvider.php
@@ -7,13 +7,15 @@ use Illuminate\Support\ServiceProvider;
 
 class GoogleCalendarServiceProvider extends ServiceProvider
 {
-    
+
     public function boot() {
-        
+        $this->publishes([
+            __DIR__.'/../config/geckob-google-calendar.php' => config_path('geckob-google-calendar.php'),
+        ], 'config');
     }
-    
+
     public function register() {
-        
+
     }
-    
+
 }
